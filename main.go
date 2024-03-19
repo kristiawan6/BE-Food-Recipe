620068e6d7dc3053622a24256269c77e23877133
William Kristiawan <williamkristiawan38@gmail.com>
2024-03-19 15:49:58 +0700

    update main.go

--- main.go
+++ main.go
@@ -6,18 +6,12 @@ import (
 	"be_food_recipe/src/routes"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
-	"log"
 )
 
 func main() {
-	
+
 	app := fiber.New()
-	
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
+
 	config.InitDB()
 	helper.Migration()
 	routes.Router(app)
