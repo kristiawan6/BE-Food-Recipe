6a64cce75c57ba7bcbd463cdacf0e12e831b8914
William Kristiawan <williamkristiawan38@gmail.com>
2024-03-20 08:49:30 +0700

    update cors

--- main.go
+++ main.go
@@ -6,11 +6,18 @@ import (
 	"be_food_recipe/src/routes"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders: "Content-Type",
+	}))
 
 	config.InitDB()
 	helper.Migration()
