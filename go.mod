8de0f90384eabf23f4a1efbe03afa8c747ceeb27
hasbiriza <hasbiriza@gmail.com>
2024-04-07 21:10:18 +0700

    mantap

--- go.mod
+++ go.mod
@@ -3,21 +3,19 @@ module be_food_recipe
 go 1.19
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.2.1
+	golang.org/x/crypto v0.21.0
 	gorm.io/driver/postgres v1.5.6
-	gorm.io/gorm v1.25.7
+	gorm.io/gorm v1.25.8
 )
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/goddtriffin/helmet v1.0.2 // indirect
-	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
 	github.com/jackc/pgx/v5 v5.5.3 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
-	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/sync v0.6.0 // indirect
 )
 
--- src/config/db.go
+++ src/config/db.go
@@ -1,17 +1,51 @@
 package config
 
 import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"log"
-	
 )
 
 var DB *gorm.DB
 
+func EnvCloudName() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file EnvCLoudname")
+	}
+	return os.Getenv("CLOUDINARY_CLOUD_NAME")
+}
+
+func EnvCloudAPIKey() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file EnvCloudAPIKey")
+	}
+	return os.Getenv("CLOUDINARY_API_KEY")
+}
+
+func EnvCloudAPISecret() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file EnvCloudAPISecret ")
+	}
+	return os.Getenv("CLOUDINARY_API_SECRET")
+}
+
+func EnvCloudUploadFolder() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file EnvCloudUploadFolder")
+	}
+	return os.Getenv("CLOUDINARY_UPLOAD_FOLDER")
+}
+
 func InitDB() {
 	url := "postgres://jglxdxwd:Yg-Dj1U_4lwhdRwsCR81eK8nnDWX9l22@floppy.db.elephantsql.com/jglxdxwd"
-	// url := os.Getenv("URL") 
+	// url := os.Getenv("URL")
 	var err error
 	DB, err = gorm.Open(postgres.Open(url), &gorm.Config{})
 	if err != nil {
